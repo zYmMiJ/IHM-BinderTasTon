75ec2654da30d58e87640c73cd26f1c2bfe233b1
zYmMiJ <avaejimmy@gmail.com>
2019-12-19 09:27:32 +1100

    header

--- src/app/pages/discuss/discuss.module.ts
+++ src/app/pages/discuss/discuss.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { DiscussPage } from './discuss.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [DiscussPage]
